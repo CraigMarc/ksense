875c2dcde99b532a5ede1c9f4215a1b3dc8abe42
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-07-24 11:46:32 -0400

    stuff

--- ksense.js
+++ ksense.js
@@ -1,12 +1,32 @@
 
-fetch('https://assessment.ksensetech.com/api/patients', {
-    method: 'GET',
-    //headers: myHeaders,
-    headers: {"x-api-key": "ak_39bd770d4512eac929930ddb075c86536d7232a3151f70c9"}
+async function fetchPatients(page) {
 
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log('API Results:', data);
-    });
+
+    let results = ""
+
+    await fetch('https://assessment.ksensetech.com/api/patients?page=' + page + '&limit=10', {
+        method: 'GET',
+        //headers: myHeaders,
+        headers: { "x-api-key": "ak_39bd770d4512eac929930ddb075c86536d7232a3151f70c9" }
+
+    })
+        .then(response => response.json())
+        .then(data => {
+            
+            return getPatientList(data)
+        });
+        
+
+}
+
+
+
+
+
+async function getPatientList(data) {
+   
+    console.log(data)
+}
+
+fetchPatients(1)
 
