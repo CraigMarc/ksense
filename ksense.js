81073051974be9a445ce851197c03317ce0bea65
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-07-24 13:16:28 -0400

    fixed functions

--- ksense.js
+++ ksense.js
@@ -1,32 +1,72 @@
 
+
 async function fetchPatients(page) {
 
 
-    let results = ""
+    try {
+        const response = await fetch('https://assessment.ksensetech.com/api/patients?page=' + page + '&limit=10', {
+            method: 'GET',
+           
+            headers: { "x-api-key": "ak_39bd770d4512eac929930ddb075c86536d7232a3151f70c9" }
 
-    await fetch('https://assessment.ksensetech.com/api/patients?page=' + page + '&limit=10', {
-        method: 'GET',
-        //headers: myHeaders,
-        headers: { "x-api-key": "ak_39bd770d4512eac929930ddb075c86536d7232a3151f70c9" }
+        })
 
-    })
-        .then(response => response.json())
-        .then(data => {
-            
-            return getPatientList(data)
-        });
+        const forecastData = await response.json();
+
+       
+        return (forecastData)
+
+    }
+    catch (error) {
+        console.error("There has been a problem with your fetch operation:", error);
         
-
+    }
 }
 
 
 
+async function getPatients() {
+
+let patientArray = []
+data1 = await fetchPatients(1)
+patientArray.push(data1)
+console.log(data1)
+
+let pages = data1.pagination.totalPages
+
+for (let i=2; i <= pages; i++) {
+    let data2 = await fetchPatients(i)
+    patientArray.push(data2)
+}
+    return patientArray
+}
+
+
+async function processData () {
+    let patientList = await getPatients()
+    console.log(patientList)
+}
+
+processData()
+
+/*
+function addPatient(data) {
+    patientArray.push(data)
+    pages = data.pagination.totalPages
+    console.log(pages)
+}
 
 
 async function getPatientList(data) {
    
-    console.log(data)
+console.log(await fetchPatients(1))
+    console.log(pages)
+    for (let i=2; i<=pages; i++) {
+        fetchPatients(i)
+    }
+
+
 }
 
-fetchPatients(1)
-
+getPatientList()
+console.log(patientArray)*/
